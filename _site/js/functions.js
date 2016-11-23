5f30b5cd85ffba8b53256309fb42175fb63d0149
FrancoFaa <franco.faa@gmail.com>
2016-11-23 15:37:20 -0500

    cmon
    damn

---
+++ _site/js/functions.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+  //
+
+});
+
+var mn = $('.topnav');
+
+$(window).scroll(function() {
+	if($(this).scrollTop()> 250) {
+		mn.addClass("topnav-scrolled");
+	}
+
+	else {
+		mn.removeClass("topnav-scrolled");
+	}
+})
