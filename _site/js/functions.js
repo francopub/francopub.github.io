a685669e62c2bc68ed97e845356015982d80878a
FrancoFaa <franco.faa@gmail.com>
2016-12-02 09:41:37 -0500

    form, js
    adding the form with js, not working locally, let’s try it on the site

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -18,3 +18,21 @@ $(window).scroll(function() {
 })
 
 */
+
+$(function() {
+  
+  // contact form animations
+  $('#contactme').click(function() {
+    $('#form-container').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#form-container");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
