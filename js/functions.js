f25a5ec3206095ee70af4782b49eb60f0f703dd3
FrancoFaa <franco.faa@gmail.com>
2017-01-04 18:01:03 -0500

    make images smaller
    make images smaller

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,6 @@
 $( document ).ready(function() {
 
-  //
-
 });
-
-/*
-var mn = $('.topnav');
-
-$(window).scroll(function() {
-	if($(this).scrollTop()> 250) {
-		mn.addClass("topnav-scrolled");
-	}
-
-	else {
-		mn.removeClass("topnav-scrolled");
-	}
-})
-
-*/
 
 $(function() {
   
