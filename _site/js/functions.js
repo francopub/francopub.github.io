7fd3694ec782a7d015f6e95bc32e0f73b77144c5
FrancoFaa <franco.faa@gmail.com>
2017-01-04 19:07:31 -0500

    file update
    files updated: script moved to body, images smallified , fonts
    localized

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -1,9 +1,4 @@
-$( document ).ready(function() {
-
-});
-
-$(function() {
-  
+$(function() {  
   var shiftWindow = function() { scrollBy(0, -135) };
   window.addEventListener("hashchange", shiftWindow);
   function load() { if (window.location.hash) shiftWindow(); }
