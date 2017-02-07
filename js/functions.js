ce58d83146b41502dcb161220a8adf40ae18bd5d
FrancoFaa <franco.faa@gmail.com>
2017-02-07 13:00:12 -0500

    js update
    js update

--- js/functions.js
+++ js/functions.js
@@ -16,10 +16,3 @@ $(function() {
     }
   });
 });
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
