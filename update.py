681ccf6d711af2ec3984abff14ca5c693f40e8f0
FrancoFaa <franco.faa@gmail.com>
2017-04-04 21:23:40 -0400

    Weekly update for Tue Apr  4 21:23:40 2017

--- update.py
+++ update.py
@@ -1,9 +1,13 @@
-import os
+import os, time
+
+today = str(time.ctime())
+
+
 
 def update():
-	os.system("jekyll build")
+	#os.system("jekyll build")
 	os.system("git add -A")
-	os.system("git commit -m 'Weekly Blog Update'")
+	os.system("git commit -m 'Weekly update for " + today + "'")
 	os.system("git push")
 	print("Job complete!")
 
