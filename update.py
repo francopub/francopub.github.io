2e06e0788f74225d64bccaf70e5be26a53f19248
FrancoFaa <franco.faa@gmail.com>
2017-04-12 22:47:15 -0400

    Weekly update for Wed Apr 12 22:47:11 2017

--- update.py
+++ update.py
@@ -1,8 +1,19 @@
 import os, time, re
 
-message = raw_input('Write blog or another string: ')
+message = raw_input('Write blog, project, or another string: ')
 
-if message == "blog":
+def update(msg):
+	os.system("jekyll build")
+	os.system("git add -A")
+	os.system(msg)
+	os.system("git push")
+
+if message == 'blog':
+	today = str(time.ctime())
+	update("git commit -m 'Weekly update for " + today + "'")
+	print("Blog post made for " + today + "!")
+
+elif message == 'project':
 	proj = raw_input("What is this week's project? ")
 	rot = open('_includes/rotation.html')
 	rot_contents = rot.read()
@@ -12,19 +23,8 @@ if message == "blog":
 	rot_contents = re.sub(content, replacement, rot_contents)
 	rot.write(rot_contents)
 	rot.close()
-
-
-today = str(time.ctime())
-
-def update(msg):
-	os.system("jekyll build")
-	os.system("git add -A")
-	os.system(msg)
-	os.system("git push")
-
-if message == 'blog':
-	update("git commit -m 'Weekly update for " + today + "'")
-	print("Blog post made for " + today + " and this week project has been updated to read " + proj)
+	update("git commit -m 'Current project changed to " + proj + "'")
+	print("Project updated to " + proj)
 
 else:
 	update("git commit -m '" + message + "'")
