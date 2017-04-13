e7aed2ef0780784ba4960f7578b06235152a9243
FrancoFaa <franco.faa@gmail.com>
2017-04-12 22:36:56 -0400

    some clean up

--- _site/update.py
+++ _site/update.py
@@ -7,7 +7,7 @@ if message == "blog":
 	rot = open('_includes/rotation.html')
 	rot_contents = rot.read()
 	rot = open('_includes/rotation.html', 'wb')
-	content = '</i> current project: (.*?)</p>'
+	content = '</i> current project: (.*?) <i'
 	replacement = '</i> current project: ' + proj + '</p>'
 	rot_contents = re.sub(content, replacement, rot_contents)
 	rot.write(rot_contents)
@@ -24,46 +24,8 @@ def update(msg):
 
 if message == 'blog':
 	update("git commit -m 'Weekly update for " + today + "'")
-	print("Blog post made for " + today + "!")
+	print("Blog post made for " + today + " and this week project has been updated to read " + proj)
 
 else:
 	update("git commit -m '" + message + "'")
 	print("Update to website made: " + message)
-
-
-'''
-
-opf_contents = opf_contents.replace('\r', '')
-opf_contents = opf_contents.replace('\r', '')
-opf_contents = opf_contents.replace('  ', ' ')
-
-## Spine regex
-
-spine_regex = '<spine toc="ncx" page-progression-direction="ltr">\n<itemref idref="toc" />\n <itemref idref="(.*?)" />\n <itemref idref="(.*?)" />'
-spine_subst = r'<spine toc="ncx" page-progression-direction="ltr"><itemref idref="cover" linear="yes" /><itemref idref="\1" /><itemref idref="\2" /><itemref idref="newsletter" /><itemref idref="toc" />'
-opf_contents = re.sub(spine_regex, spine_subst, opf_contents)
-
-## metadata regex 
-
-metadata_regex = '<dc:identifier id="bookid"></dc:identifier>\n<dc:date>(.*?)</dc:date>\n<dc:rights></dc:rights>'
-metadata_subst = r'<dc:date>\1</dc:date><dc:identifier id="bookid">' + str(isbn) + r'</dc:identifier><dc:creator id="mainauthor1">' + str(firstName) + ' ' + str(lastName) + r'</dc:creator><meta refines="#mainauthor1" property="role" scheme="marc:relators">aut</meta><meta refines="#mainauthor1" property="file-as">' + str(lastName) + r', ' + str(firstName) + r'</meta><meta refines="#mainauthor1" property="display-seq">1</meta><dc:publisher>Adams Media</dc:publisher><dc:rights>WORLD</dc:rights>'
-opf_contents = re.sub(metadata_regex, metadata_subst, opf_contents)
-
-opf.write(opf_contents)
-opf.close()
-'''
-
-
-
-'''
-digit = re.search(r'\[\d\]', rot_contents)
-digit = int(digit.group(0)[1])
-
-if digit == 9:
-	digit = 0
-elif digit >= 0:
-	digit += 1
-
-print digit
-
-'''
