5eee00d2919a414075ec4c50b88ec4a44f6d44d9
Franco Alvarado <franco@Francos-MacBook-Pro.local>
2018-01-02 17:22:41 -0500

    uploading zork game
    uploading zork game

---
+++ zork.js
@@ -0,0 +1,19 @@
+var rooms =  {
+	"room0": "<p>You are in a dark cold place and you see a light to the <b>north</b> and you hear the sound of running water to the <b>west</b>.</p>"
+}
+
+
+
+$(document).ready(function(){
+	$('#game-text').prepend(rooms['room0']);
+	$(document).keypress(function(key){
+		if(key.which === 13 && $('#user-input').is(':focus')) {
+			var value = $('#user-input').val();
+			alert(value);
+		}
+		
+
+
+	})
+
+});
