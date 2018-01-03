6f281e5ecd2d28efae7e18d77d0ef939010a2893
Franco A. Alvarado <franco.faa@gmail.com>
2018-01-03 15:36:15 -0500

    Update rooms.js

--- js/rooms.js
+++ js/rooms.js
@@ -1,26 +1,22 @@
 var rooms = {
-    "start": {
-        "description": "You are in a dark, cold place and you see a light to <b>north</b>\
-     and you hear the sound of running water to the <b>west</b>",
+    "a1": {
+        "description": "You find yourself in a strange place. It is dark in here and strangely warm. You wait for your eyes to adjust to the darkness, and you can see two doors, one to the south and one to the east.",
         "directions": {
-            "north": "clearing1",
-            "west": "bridge1"
+            "south": "b1",
+            "east": "a2"
         },
         "items": {
             "sword": "You see a shimmering light in a bush" // 
         }
 
     },
-    "clearing1": {
-        "description": "You arrive to a clearing, you see a lighthouse to the <b>north</b>\
-     and there is a strange smell coming from the <b>east</b>",
+    "a2": {
+        "description": "You enter into a chapel. There are pews arranged in front of an altar. Stained glass windows go up and down the aisle, but the ceiling is very low so the windows are rather unimpressive. The altar has a large banner behind it with the image of a strange god you do not recognize. To the west is a wooden door.",
         "directions": {
-            "south": "start",
-            "north": "lighthouse",
-            "east": "trolls"
+            "west": "a1",
         }
     },
-    "lighthouse": {
+    "a3": {
         "description": "You arrive to the lighthouse and walk up to the door. A strange old lady\
      opens the door. What do you do?",
         "directions": {
@@ -30,21 +26,21 @@ var rooms = {
             "old lady": "The old lady emits a strange noise and attacks you"
         }
     },
-    "trolls": {
+    "a4": {
         "description": "You arrive to another clearing, there are some trolls roasting some mysterious meat\
      They haven't seen you yet. What do you do?",
         "directions": {
             "west": "clearing1"
         }
     },
-    "bridge1": {
+    "b1": {
         "description": "You see a river and there is a bridge to the <b>west</b>",
         "directions": {
             "east": "start",
             "west": "bridge2"
         }
     },
-    "bridge2": {
+    "b2": {
         "description": "You try to cross the bridge but a troll jumps out and bites your leg!",
         "directions": {
             "east": "bridge1"
